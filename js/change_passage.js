4a4b1a63b135a87baa2a24ad8dcc5289a2d3d0aa
syachi <1040899365@qq.com>
2015-11-08 23:33:05 +0800

    added function of checking detail for change_passage page.

--- js/change_passage.js
+++ js/change_passage.js
@@ -4,5 +4,15 @@ $(function(){
 		$(tr).toggleClass("article-row-selected");
 		$(this).find(".fa-stack-1x").toggleClass("fa-circle");
 		$(this).find(".fa-stack-1x").toggleClass("fa-check");
+		$(tr).siblings().removeClass("article-row-selected");
+		$(tr).siblings().find(".fa-stack-1x").removeClass("fa-check");
+		$(tr).siblings().find(".fa-stack-1x").addClass("fa-circle");
+	});
+});
+
+$(function(){
+	$("[toggle=collapse]").click(function(){
+		var target = $($(this).attr("target"));
+		$(target).toggle(500);
 	});
 });
