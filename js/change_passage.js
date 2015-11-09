214246cbc653a4edf170b70c818f2b6b883288d1
unknown <Linkin_HearT@sjtu.edu.cn>
2015-11-09 21:39:40 +0800

    add some more pages: choose detailed pages

--- js/change_passage.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$(".article-row th:first-child, .article-row th:nth-child(2)").click(function(){
-		var area = $(this).parent().children(".fa-stack");
-		// alert(area);
-		$(this).parent().toggleClass("article-row-selected");
-		$(area).find(".fa-stack-1x").toggleClass("fa-circle");
-		$(area).find(".fa-stack-1x").toggleClass("fa-check");
-	});
-});
---
+++ js/handle_question.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+	$(".left-bar .scroll-container").rollbar({
+		scroll: 'vertical',
+		autoHide: true,
+		sliderSize: "20%",
+		pathPaddingTop: '62px',
+		pathPaddingBottom: '-10px',
+	});
+
+	$(".right-bar .scroll-container").rollbar({
+		scroll: 'vertical',
+		autoHide: true,
+		sliderSize: "20%",
+		pathPaddingTop: '10px',
+		pathPaddingBottom: '-10px',
+	});
+
+	$(function(){
+		$(".article-row th:first-child, .article-row th:nth-child(2)").click(function(){
+			var area = $(this).parent().children(".fa-stack");
+			// alert(area);
+			$(this).parent().toggleClass("article-row-selected");
+			$(area).find(".fa-stack-1x").toggleClass("fa-circle");
+			$(area).find(".fa-stack-1x").toggleClass("fa-check");
+
+			$(".left-bar").load("/teacher_accessment_0.html")
+
+		});
+	});
+
+	$(function(){
+		$(".question_list li").click(function(){
+			$(this).toggleClass("question_selected");
+		});
+	});
+
+});
