77e2f6463f758abfde624a40493c392ebae32415
unknown <Linkin_HearT@sjtu.edu.cn>
2015-11-09 22:04:28 +0800

    small changes to change_passage

--- js/change_passage.js
+++ js/change_passage.js
@@ -1,8 +1,11 @@
 $(function(){
 	$(".article-row th:first-child, .article-row th:nth-child(2)").click(function(){
 		var area = $(this).parent().children(".fa-stack");
-		// alert(area);
+		// var tr = $(this).parent();
+		// alert(tr);
+
 		$(this).parent().toggleClass("article-row-selected");
+		$(area).toggleClass(".fa-check");
 		$(tr).siblings().removeClass("article-row-selected");
 		$(tr).siblings().find(".fa-stack-1x").removeClass("fa-check");
 		$(tr).siblings().find(".fa-stack-1x").addClass("fa-circle");
@@ -13,5 +16,7 @@ $(function(){
 	$("[toggle=collapse]").click(function(){
 		var target = $($(this).attr("target"));
 		$(target).toggle(500);
+		// alert($(this).text());
+		$(this).text($(this).text() == "详情" ? "收起" : "详情")
 	});
 });
